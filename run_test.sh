7a68480c8dc7895bfd5abd0e6f58f5a4ea4816e1
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2022-06-17 18:06:59 +0200

    added latest results

--- run_test.sh
+++ run_test.sh
@@ -1,20 +1,48 @@
 # yeast model vs yeast dataset
-python model_test.py \
---model model_save_yeast.h5 \
---dic yeast/preprocessed/protein.dictionary.tsv \
---act yeast/preprocessed/protein.actions.tsv \
---csv results/yeast_yeast.csv
+# python model_test.py \
+# --model model_save_yeast.h5 \
+# --dic yeast/preprocessed/protein.dictionary.tsv \
+# --act yeast/preprocessed/protein.actions.tsv \
+# --csv results/yeast_yeast.csv
 
 # yeast model vs ath dataset
-python model_test.py \
---model model_save_yeast.h5 \
---dic ath/protein.dictionary.tsv \
---act ath/protein.actions.tsv \
---csv results/yeast_ath.csv
+# python model_test.py \
+# --model model_save_yeast.h5 \
+# --dic ath/protein.dictionary.tsv \
+# --act ath/protein.actions.tsv \
+# --csv results/yeast_ath.csv
 
 # MS model vs ath dataset
+# python model_test.py \
+# --model model_save_MS.h5 \
+# --dic ath/protein.dictionary.tsv \
+# --act ath/protein.actions.tsv \
+# --csv results/ms_ath.csv
+
+# MS model 2 vs ath dataset
+# python model_test.py \
+# --model model_save_MS10yeast_30.h5 \
+# --dic ath/protein.dictionary.tsv \
+# --act ath/protein.actions.tsv \
+# --csv results/ms_2_ath.csv
+
+# yeast model 2 vs ath dataset
+# python model_test.py \
+# --model model_save_yeast_45ep.h5 \
+# --dic ath/protein.dictionary.tsv \
+# --act ath/protein.actions.tsv \
+# --csv results/yeast_ath.csv
+
+# MS model 2 vs ath dataset
+# python model_test.py \
+# --model model_save_MS10yeast_30.h5 \
+# --dic ath/protein.dictionary.tsv \
+# --act ath/protein.actions.tsv \
+# --csv results/ms_2_ath.csv
+
+# MS model 3 vs ath dataset
 python model_test.py \
---model model_save_MS.h5 \
+--model model_save_MSfiltered25_10ep.h5 \
 --dic ath/protein.dictionary.tsv \
 --act ath/protein.actions.tsv \
---csv results/ms_ath.csv
+--csv results/ms_3_ath.csv
